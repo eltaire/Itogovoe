74c7b7413262fdd90416f9a238ee48aecfff788f
Elta <eltaire@gmail.com>
2022-11-07 13:47:26 +0300

    first stage

---
+++ II/Program.cs
@@ -0,0 +1,17 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. 
+		
+string[] array = AskArray();
+string[] result = FindArray(array, 3);
+
+string[] FindArray(string[] input, int n) {
+    string[] output = new string[CountProcedure(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
